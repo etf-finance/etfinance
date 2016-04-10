b5d347d730840dce5baf1b2f0b88e6325a3993d2
borenstejn <jeromeborenstejn@gmail.com>
2016-04-10 11:38:51 +0200

    without plucking

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,16 @@
 class WelcomeController < ApplicationController
   def index
+  	quotes = Quote.all
   	@quotes = Quote.pluck(:created_at, :value)
   	@quotes.each do |el|
   		el[1] = el[1].to_i
   		# el[0] = el[0].strftime("%H:%M:%S%z")
   	end
+  	{20.day.ago => 5, 1368174456 => 4, "2013-05-07 00:00:00 UTC" => 7}
+  	@hash = Hash.new
+  	quotes.each do |q|
+  		@hash[q.created_at.strftime("%H:%M")] = q.value.to_i
+  	end
+  	ap @hash
   end
 end
