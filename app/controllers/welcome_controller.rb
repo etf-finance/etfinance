978d83157980f1e18ce661ed3e98dd76c304b92a
borenstejn <jeromeborenstejn@gmail.com>
2016-04-10 10:31:43 +0200

    chartkick intalled

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+  	@hash = User.group_by_day(:created_at).count
+  	# @hash = Hash.new
+  	# @hash[:test] = 2
+  	# @hash[:test2] = 10
   end
 end
