992e6dd6075c22f9209d90c477e0cef7574033b0
borenstejn <jeromeborenstejn@gmail.com>
2016-04-11 13:09:18 +0200

    strip checkout bug

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -7,11 +7,22 @@ class SubscribersController < ApplicationController
 	end
 
 	def create
-		raise
+		token = params[:stripeToken]
+
+		customer = Stripe::Customer.create(
+			card: token,
+			plan: 1020,
+			email: current_user.email
+			)
+
+		current_user.subscribed = true
+		current_user.stripe_id = customer.id
+		current_user.save
+
+		redirect_to root_path
 	end
 
 	def update
-		raise
 		token = params[:stripeToken]
 
 		customer = Stripe::Customer.create(
