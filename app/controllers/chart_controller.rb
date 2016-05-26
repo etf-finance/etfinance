2308149b2c038ffd69073bf2bf9f5bffbfd3ced5
borenstejn <jeromeborenstejn@gmail.com>
2016-05-26 13:57:30 +0200

    bug fixed

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -128,7 +128,8 @@ class ChartController < ApplicationController
 
       element = @chart_data.find{|x| x["quotes_array"].present? }
 
-      @symbols_array = element["quotes_array"]
+      @symbols_array = element["quotes_array"] || ["SPY", "VXX", "VXZ", "XIV", "ZIV"]
+
 
       @premium = true
 
